fb8023322d02b50b494930542a641de48d08e68b
diaz20px <1597037@senati.pe>
2026-04-20 16:40:05 -0500

    configuracion app

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
+// const express = require("express");
+// const path = require("path");
+
+// const app = express ();
+
+// app.set("view engine","ejs");
+// app.set("views",path.join(__dirname,"views"));
+
+// app.use(express.static("public"));
+// app.use(express.static(path.join(__dirname,"public"))),
+// app.use(express.urlencoded({extended: true}));
+// app.use(express.json());
+
+// const rutas = require("./routes/rutas");
+// app.use("/",rutas);
+
+// app.listen(3000, () => {
+//     console.log("Servidor en http://localhost:3000");
+// });
+
+// =========================================
+
 const express = require("express");
 const path = require("path");
 
-const app = express ();
+const app = express();
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static("public"));
-app.use(express.static(path.join(__dirname,"public"))),
-app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const rutas = require("./routes/rutas");
-app.use("/",rutas);
+app.use("/", rutas);
 
 app.listen(3000, () => {
     console.log("Servidor en http://localhost:3000");
 });
-
